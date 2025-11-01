5f6866bfb277d22cc9f5140ec89d00de6a48f96d
demonlovest <Lixin070514@126.com>
2025-11-01 20:10:57 +0800

    Update 3_work3.c

--- 3_work3.c
+++ 3_work3.c
@@ -3,38 +3,17 @@
 //李鑫
 #include <stdio.h>
 int main() {
-    int a, b;
-    char op;
-    double result;
-    printf("请输入第一个数：");
-    scanf("%d",&a);
-    printf("请输入第二个数：");
-    scanf("%d",&b);
-    printf("请输入运算符：");
-    scanf(" %c",&op);
-    switch(op){
-        case'+':
-        result=a+b;
-        break;
-        case'-':
-        result=a-b;
-        break;
-        case'*':
-        result=a*b;
-        break;
-        case'/':
-        if(b !=0) {
-            result=(double)a/b;
-        }else {
-                printf("除数不能为0!\n");
-                return 1;  
-            }
-            break;
-        default:
-            printf("无效的运算符！\n");
-            return 1; 
+    printf("100到999之间的水仙花数有:\n");
+    int num=100;
+    while (num<=999) {
+    int unit = num % 10;
+    int ten = (num / 10 ) % 10;
+    int hundred = num / 100;
+    int sum = unit*unit*unit+ten*ten*ten+hundred*hundred*hundred;
+    if ( sum == num ) {
+        printf("%d ", num);
     }
-    
-    printf("%d %c %d = %g\n",a,op,b,result);
+    num++;    
+    }
     return 0;
-} 
+}
