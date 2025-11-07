aac67a3520d4d4c56137a5d9acf94c78ee2d64f7
demonlovest <Lixin070514@126.com>
2025-11-07 16:34:52 +0800

    Update 1_work1.c

--- 1_work1.c
+++ 1_work1.c
@@ -1,14 +1,27 @@
 //202510306114
-//Lixn070514@126.com
+//Lixin070514@126.com
 //李鑫
 #include <stdio.h>
-
-int main()
-{
-    float celsius,fahrenheit;
-    printf("请输入摄氏温度：");
-    scanf("%f",&celsius);
-    fahrenheit= celsius*9/5+32;
-    printf("华氏温度为:%.1f\n",fahrenheit);
+int main () {
+    int mat[3] [3],trans[3] [3];
+    int i,j;
+    printf("请输入矩阵：");
+    for (i=0;i<3;i++) {
+        for (j=0;j<3;j++) {
+            scanf("%d", &mat[i] [j]);
+        }
+    }
+    for (i=0;i<3;i++) {
+        for (j=0;j<3;j++) {
+            trans[j] [i] = mat[i] [j];
+        }
+    }
+    printf("转置后的矩阵为：");
+    for (i=0;i<3;i++) {
+        for (j=0;j<3;j++) {
+            printf("%d ", trans[i] [j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
