0a5d45b1b53d3f0aca95cb1883e560c810679094
demonlovest <Lixin070514@126.com>
2025-11-07 16:36:27 +0800

    Update 4_work4.c

--- 4_work4.c
+++ 4_work4.c
@@ -2,21 +2,12 @@
 //Lixin070514@126.com
 //李鑫
 #include <stdio.h>
-int main() {
-    int arr[5];
-    int sum=0;
-    printf("请输入四个整数：");
-    for (int i=0;i<4;i++) {
-        scanf("%d", &arr[i]);
-        sum += arr[i];
-    }
-    arr[4]=sum;
-    for (int i=0;i<5;i++) {
-        if ( i==4 ) {
-        printf("%d", arr[i]);
-        } else {
-            printf("%d ", arr[i]);
-        }
-    }
+int assd(int a1,int an,int step) {
+    int count =((an-a1)/ step)+1;
+    return count*(a1+an)/2;
+}
+int main () {
+    int sum=assd(1,100,1);
+    printf("%d\n",sum);
     return 0;
 }
